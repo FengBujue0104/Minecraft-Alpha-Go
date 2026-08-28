16569e41ab71132baa23bef2a1d4030cfeccb2f0
root <root@nn7aa56kq6jbuhr.rand>
2026-08-28 11:16:32 +0000

    Fix canary B build: //export needs its own file with declaration-only prelude

--- android/canary/canaryb/tick.go
+++ android/canary/canaryb/tick.go
@@ -3,6 +3,13 @@
 // tick.go：//export 必须单独成文件，原因见 glue.go 头部注释。
 package main
 
+/*
+// 仅声明：定义在 glue.go；export 文件的 prelude 会被复制进 _cgo_export.c，
+// 放定义会符号重复。
+extern void finishActivity();
+*/
+import "C"
+
 import "time"
 
 func main() {}
